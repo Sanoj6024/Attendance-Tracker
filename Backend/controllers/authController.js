b4b43612db9cb316e4913e3477d196878b7534c6
Sanoj6024 <sanojshreyasdeo@gmail.com>
2025-12-28 13:04:22 +0530

    Day 3: Authentication and JWT login

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,76 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// REGISTER
+exports.registerUser = async (req, res) => {
+  try {
+    const { fullName, email, password, role, batch, semester } = req.body;
+
+    // Check if user exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+    const user = await User.create({
+      fullName,
+      email,
+      password: hashedPassword,
+      role,
+      batch,
+      semester,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// LOGIN
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // JWT Payload
+    const payload = {
+      id: user._id,
+      fullName: user.fullName,
+      role: user.role,
+      batch: user.batch,
+      semester: user.semester,
+    };
+
+    // Sign token
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.json({
+      token,
+      user: payload,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- Backend/server.js
+++ Backend/server.js
@@ -11,6 +11,7 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", require("./routes/authRoutes"));
 
 app.get("/", (req, res) => {
   res.send("Attendance Tracker API Running");
