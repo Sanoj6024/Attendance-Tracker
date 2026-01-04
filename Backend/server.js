af86474635f7197e01903589b672db4ae0335e90
Sanoj6024 <sanojshreyasdeo@gmail.com>
2026-01-04 13:55:46 +0530

    backend for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -4,25 +4,25 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const User = require("./models/User");
 
-
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/subjects", require("./routes/subjectRoutes"));
 app.use("/api/attendance", require("./routes/attendanceRoutes"));
-
 app.use("/api/users", require("./routes/userRoutes"));
-
-
 
 app.get("/", (req, res) => {
   res.send("Attendance Tracker API Running");
 });
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
+/* ✅ STEP 1: SAFE PORT FOR DEPLOYMENT */
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
 );
