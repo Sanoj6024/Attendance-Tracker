2c5e38718816f6fc8658c488636f54acf5d4896d
Sanoj shreyas deo <119153526+Sanoj6024@users.noreply.github.com>
2026-01-10 14:32:34 +0530

    axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://attendease-backend-riez.onrender.com/api",
+  baseURL: "https://attendance-tracker-5ptj.onrender.com/api",
 });
 
 instance.interceptors.request.use(
